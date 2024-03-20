7b02a45a1ab3eec94015c617fd01d0543ce3fba7
danbplus <danbplus@DESKTOP-VVS2EGI>
2024-03-20 09:56:41 +0900

    commit

---
+++ src/test/java/com/danbplus/ScheduleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danbplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
