6fdb15fb702683d91513db770dec9818a888ef9d
danbplus <daegyeong.lim@danbplus.com>
2024-04-16 16:21:34 +0900

    2024-04-16 LDG  SpringBootServletInitializer 상속

--- src/main/java/com/danbplus/ScheduleProjectApplication.java
+++ src/main/java/com/danbplus/ScheduleProjectApplication.java
@@ -1,12 +1,17 @@
 package com.danbplus;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ScheduleProjectApplication {
+public class ScheduleProjectApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ScheduleProjectApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ScheduleProjectApplication.class, args);
